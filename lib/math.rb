8358f5040655b3bf4fe391a2e951c9c716a98ff8
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-07 19:23:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,15 @@ end
 addition(4, 5)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
 
 def division(num1, num2)
-
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  num1 ** num2
 end
 
 def modulo(num1, num2)
